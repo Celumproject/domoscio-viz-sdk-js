5edea9d54c465f37cb8cd643c9ca3a8eda97f73b
MatthieuMu <matthieu.mugnier@hotmail.fr>
2018-03-29 16:07:00 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,94 @@
+# DomoscioJS
+
+This README would document whatever steps are necessary to get the DomoscioJS SDK up and running.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+DomoscioJS requires the jQuery Javascript library. Make sure to load jquery.js file before loading DomoscioJS.
+
+```
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+```
+
+### Installing
+
+Place the following <script>s near the end of your pages, right before the closing </body> tag, to enable them. jQuery must come first, then DomoscioJS, and then your script.
+
+```
+<script src="https://rawgit.com/Celumproject/domoscio_js/master/v1/domosciojs.min.js"></script>
+```
+
+Then you have to configure the DomoscioJS object like bellow with your credentials to access your enabled APIs. Refer to the API documentation for details:
+https://domoscio.com/wiki/doku.php?id=api2:start
+
+```
+DomoscioJS.configuration = { 
+    preproduction: true,
+    version: API_VERSION,
+    client_id: YOUR_INSTANCE_ID,
+    client_passphrase: "YOUR_ACCESS_TOKEN"
+}
+```
+
+| Key  | Type | Description |
+| ------------- | ------------- | ------------- |
+| preproduction  | `boolean` | true is for development (use the preproduction Domoscio API) and false for production |
+| version  | `integer` | the current version of Domoscio API with latest features is 2 |
+| client_id  | `integer` | this is your instance_id, required for access to your data |
+| client_passphrase  | `string` | client_passphrase is your secret key, this token is paired with your client_id |
+
+## Samples
+
+Simple yet flexible JavaScript request for Domoscio API.
+
+### Fetch
+
+Fetch all object corresponding with the parameters :
+
+```
+DomoscioJS.Student.fetch({uid: "Example"})
+```
+
+### Find
+
+Find the object corresponding with the id :
+
+```
+DomoscioJS.Student.find({id: "Example"})
+```
+
+### Create
+
+Create an object :
+
+```
+DomoscioJS.Student.create({uid: "Example", active: true})
+```
+
+### Utils
+
+Some utils routes :
+
+```
+DomoscioJS.GameplayUtil.util("get_review_progress", { student_id: id, knowledge_node_id: id })
+```
+
+## Deployment
+
+To deploy this on a live system, use the <script> tag bellow : 
+
+```
+<script src="https://cdn.rawgit.com/Celumproject/domoscio_js/160e555c/v1/domosciojs.min.js"></script>
+```
+
+## Versioning
+
+Currently v1.0.3
+
+## Authors
+
+See the list of contributors (https://github.com/Celumproject/domoscio_js/contributors)
---
+++ v1/domoscioviz.js
@@ -0,0 +1,183 @@
+/* DomoscioViz v1.0.3 (Custom Build)
+ * Dependencies : jQuery (https://jquery.com/)
+ * Minify with https://skalman.github.io/UglifyJS-online/
+ * Build: https://domoscio.com/ | https://github.com/Celumproject/domoscio_js
+ */
+
+//#######################################################//
+//######              UTILS FUNCTIONS              ######//
+//#######################################################//
+
+if ("undefined" == typeof jQuery)
+    throw new Error("DomoscioViz requires jQuery");
+
+// Errors
+function errors(type = 'token') {
+    var errors = "";
+    switch (type) {
+        case 'token':
+            errors = 'Invalid API Token';
+            break;
+        default:
+            errors = 'Invalid API Token';
+            break;
+    }
+    return errors;
+}
+
+function viz_loader(){
+    return "<img src='https://domoscio2-demo.herokuapp.com/images/ripple.svg' alt='loader' id='viz_loader'>";
+}
+
+// Build http call
+function http_calls(ressource = "") {
+    var host_url = DomoscioViz.configuration.preproduction == true ? "https://domoscio-viz-engine.herokuapp.com" : "http://localhost:3002";
+    var instance = DomoscioViz.configuration.client_id;
+    var token = DomoscioViz.authorization_token(DomoscioViz.configuration.client_passphrase);
+
+    if (token == false)
+        return token
+    else
+        var url = host_url;
+    return url;
+}
+
+// Redirect to the right route
+function ressource(name) {
+    // If name isn't a string 
+    if (typeof name !== "string") {
+        name = name.toString();
+        var pos = name.indexOf(" ");
+        name = name.substring(pos + 1);
+        pos = name.indexOf("(");
+        name = name.substring(0, pos);
+    }
+    // ----------------------
+    array = name.split(/(?=[A-Z])/);
+    var add_underscore = "";
+    for (i = 0; i < array.length; i++) {
+        (i < array.length - 1) ? add_underscore += array[i] + "_" : add_underscore += array[i];
+    }
+    var ressource = "";
+    ressource += add_underscore.toLowerCase() + "s";
+    return ressource;
+}
+
+//#######################################################//
+//######               HTTP REQUEST                ######//
+//#######################################################//
+
+// Méthode Util
+// function get_url(route, filters = {}) {
+//     var object = ressource(this.child.parent.name);
+//     var token = DomoscioViz.authorization_token(DomoscioViz.configuration.client_passphrase);
+//     var url = http_calls(object) + "/" + route;
+//     if (url !== false) {
+//         var data = "";
+//         Object.keys(filters).map(function (key, index) {
+//             if (index == 0)
+//                 data += key + "=" + filters[key];
+//             else
+//                 data += "&" + key + "=" + filters[key];
+//         });
+//         var result = "";
+//         var settings = {
+//             async: true,
+//             crossDomain: true,
+//             url: url,
+//             method: "POST",
+//             headers: {
+//                 // 'Content-Type': "application/json",
+//                 'ClientID': DomoscioViz.configuration.client_id,
+//                 'ClientPassphrase': token,
+//                 'Cache-Control': "no-cache"
+//             },
+//             processData: false,
+//             data: data
+//             // contentType: "application/json; charset=utf-8",
+//         }
+
+//         $.ajax(settings).done(function (response) {
+//             console.log(response);
+//         });
+//         return result;
+//     }
+// };
+function get_url(route, filters = {}) {
+    var object = ressource(this.child.parent.name);
+    var token = DomoscioViz.authorization_token(DomoscioViz.configuration.client_passphrase);
+    var url = http_calls(object) + "/" + route;
+    if (url !== false) {
+        var data = "";
+        Object.keys(filters).map(function (key, index) {
+            if (index == 0)
+                data += key + "=" + filters[key];
+            else
+                data += "&" + key + "=" + filters[key];
+        });
+        var result = "";
+        $.ajax({
+            headers: {
+                'ClientID': DomoscioViz.configuration.client_id,
+                'ClientPassphrase': token
+                // 'Content-Type': 'application/json'
+            },
+            method: "POST",
+            url: url,
+            data: data,
+            async: false,
+            // dataType: "json",
+            crossDomain: true,
+            processData: false,
+            complete: function (e, statut) {
+                result = JSON.parse(e.responseText)["url"];
+            },
+            error: function (e, statut) {
+                console.error("We're sorry, but something went wrong. (500)");
+            }
+        });
+        return result;
+    }
+};
+
+//#######################################################//
+//######                    LIB                    ######//
+//#######################################################//
+
+let Chart = {
+    name: "Chart",
+    child: {},
+    get_url,
+    init: function () {
+        this.child.parent = this;
+        delete this.init;
+        return this;
+    }
+}.init();
+
+//#######################################################//
+//######                DomoscioViz                ######//
+//#######################################################//
+
+DomoscioViz = {
+
+    // Configuration
+    "configuration": {
+        preproduction: true,
+        client_id: 0,
+        client_passphrase: ""
+    },
+
+    // Token
+    "authorization_token": function (token) {
+        if (token == null) {
+            console.error(JSON.stringify(errors()));
+            return false;
+        }
+        else
+            return token;
+    },
+
+    Chart
+
+}
